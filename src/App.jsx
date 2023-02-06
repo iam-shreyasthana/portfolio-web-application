fd0ec5b35af8db51337db136729ea9ba002d0544
Shrey Asthana <shreyasthana545@gmail.com>
2023-02-06 18:05:06 +0530

    linked components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Header from "./components/header/header";
+import Nav from "./components/nav/nav";
+import About from "./components/about/about";
+import RecentWork from "./components/recentWork/recentWork";
+import Services from "./components/services/services";
+import Experience from "./components/experience/experience";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer"
 
 const App = () => {
   return (
     <>
-        <header/>
+        <Header />
+        <Nav />
+        <About />
+        <RecentWork />
+        <Experience />
+        <Services />
+        <Contact />
+        <Footer />
     </>
   )
 }

4aa4a3d15753c50b1f39c95ce3a65d853fb212b3
Shrey Asthana <shreyasthana545@gmail.com>
2023-02-07 00:17:37 +0530

    added nav bar icons

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './nav.css';
+import {BiHome} from 'react-icons/bi';
+import {AiOutlineUser} from 'react-icons/ai'
+import {GoBook} from 'react-icons/go';
+import {RiServiceLine} from 'react-icons/ri';
+import {AiOutlineMessage} from 'react-icons/ai';
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#'><BiHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><GoBook /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><AiOutlineMessage /></a>
+    </nav>
   )
 }
 

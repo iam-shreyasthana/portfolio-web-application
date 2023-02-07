10b944422628ba0c6c3db5665e5c91ecfdc3f6fc
Shrey Asthana <shreyasthana545@gmail.com>
2023-02-08 01:59:03 +0530

    UPDATE: contact section updated with the styling

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import "./contact.css";
+import {AiOutlineMail} from 'react-icons/ai';
+import {RiMessengerLine} from 'react-icons/ri';
+import {AiOutlineWhatsApp} from 'react-icons/ai';
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>shreyasthana545@gmail.com</h5>
+            <a href='mailto:shreyasthana545@gmail.com' target='_blank'>Send a Message</a>
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Shrey Asthana</h5>
+            <a href='https://m.me/iam.shreyasthana' target='_blank'>Send a Message</a>
+          </article>
+
+          <article className='contact__option'>
+            <AiOutlineWhatsApp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+91-8595-849-277</h5>
+            <a href='https://api.whatsapp.com/send?phone=918595849277' target='_blank'>Send a Message</a>
+          </article>
+        </div>
+        {/*............ Contact Options End ...........*/}
+        <form action=''>
+            <input type='text' name='name' placeholder='Your Full Name' required />
+            <input type='email' name='email' placeholder='Your Email' required/>
+            <textarea name='message' rows='7' placeholder='Your Message' required ></textarea>
+            <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 

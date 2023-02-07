fc463421d01f910b619332bdf812cc1cae8e89e5
Shrey Asthana <shreyasthana545@gmail.com>
2023-02-07 09:06:18 +0530

    services i offer completed and changes in css

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import './services.css';
+import {BsCheck2All} from 'react-icons/bs';
 
 const Services = () => {
   return (
-    <div>services</div>
+    <section id='services'>
+      <h5>What I Offer?</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck2All className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2All className='service__list-icon' />
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+      
+
+    </section>
   )
 }
 

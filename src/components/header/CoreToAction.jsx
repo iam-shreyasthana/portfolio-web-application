a8195f92231406a0bab0608fc186ae1268c0d8ad
Shrey Asthana <63008029+iam-shreyasthana@users.noreply.github.com>
2024-05-13 15:16:01 +0530

    resume changed

--- src/components/header/CoreToAction.jsx
+++ src/components/header/CoreToAction.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from "../../assets/Shrey Asthana Resume.pdf";
+import CV from "../../assets/Shrey Asthana Resume A24.pdf";
 
 const CoreToAction = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Download Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )

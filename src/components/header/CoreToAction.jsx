4debe8c57bf0a55050a9189e48ce83ad12687016
Shrey Asthana <63008029+iam-shreyasthana@users.noreply.github.com>
2024-05-13 15:39:05 +0530

    download resume changed to view resume

--- src/components/header/CoreToAction.jsx
+++ src/components/header/CoreToAction.jsx
@@ -4,7 +4,7 @@ import CV from "../../assets/Shrey Asthana Resume A24.pdf";
 const CoreToAction = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download Resume</a>
+        <a href={CV} download className='btn'>View Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
